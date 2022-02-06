7d5086ddd73773019d74be1ac234ca7b027b3975
Fábio Matias <fabio_mcm2@hotmail.com>
2022-02-06 23:43:43 +0000

    feat(contract): enable rinkeby deploys

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +9,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
+    },
+    rinkeby: {
+      network_id: 4,
+      provider: () =>
+        new HDWalletProvider(process.env.SEED_PHRASE, process.env.ALCHEMY_KEY),
     },
   },
   compilers: {
