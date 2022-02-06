5e93b2efe85f9f0eda5c85154dbc4f0ba6d703d8
Fábio Matias <fabio_mcm2@hotmail.com>
2022-02-06 23:44:27 +0000

    chore(contract): compile contracts to client

--- truffle-config.js
+++ truffle-config.js
@@ -4,6 +4,7 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+  contracts_build_directory: "./client/contracts",
   networks: {
     development: {
       host: "127.0.0.1",
