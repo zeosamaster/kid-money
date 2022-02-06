ce39f9dce0dfb091903ca1a8454e40ecaeb96473
Fábio Matias <fabio_mcm2@hotmail.com>
2022-02-06 00:36:32 +0000

    chore(ui): extract home components

---
+++ client/components/Header.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Header() {
+  return (
+    <>
+      <h1 className={styles.title}>Welcome to Kid Money!</h1>
+
+      <p className={styles.description}>
+        Here you&apos;ll be able to manage your money:
+        <br />
+        <Image
+          height={24}
+          width={24}
+          alt="deposit"
+          src="/icons/deposit.svg"
+        />{" "}
+        deposit +{" "}
+        <Image
+          height={24}
+          width={24}
+          alt="withdraw"
+          src="/icons/withdraw.svg"
+        />{" "}
+        withdraw
+        <br />
+        <Image height={24} width={24} alt="save" src="/icons/save.svg" /> save +{" "}
+        <Image
+          height={24}
+          width={24}
+          alt="compound"
+          src="/icons/compound.svg"
+        />{" "}
+        compound
+      </p>
+    </>
+  );
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
+import Actions from "../components/Actions";
+import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -13,55 +13,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className={styles.title}>Welcome to Kid Money!</h1>
-
-      <p className={styles.description}>
-        Here you'll be able to manage your money:
-        <br />
-        <Image height={24} width={24} src="/icons/deposit.svg" /> deposit +{" "}
-        <Image height={24} width={24} src="/icons/withdraw.svg" /> withdraw
-        <br />
-        <Image height={24} width={24} src="/icons/save.svg" /> save +{" "}
-        <Image height={24} width={24} src="/icons/compound.svg" /> compound
-      </p>
+      <Header />
 
       <div className={styles.grid}>
-        <Link href="/account/deposit">
-          <a className={styles.card}>
-            <h2>Deposit &rarr;</h2>
-            <p>Deposit your tokens to be able to save & compound 🏦</p>
-          </a>
-        </Link>
-
-        <Link href="/account/withdraw">
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Withdraw &rarr;</h2>
-            <p>Withdraw tokens to convert them to money you can spend 💸</p>
-          </a>
-        </Link>
-
-        <Link href="/account/save">
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Save &rarr;</h2>
-            <p>Save your tokens to get even more tokens! 💰</p>
-          </a>
-        </Link>
-
-        <Link href="/account/unsave">
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Unsave &rarr;</h2>
-            <p>
-              Take your tokens out of savings so you can decide what else to do
-              with them 💱
-            </p>
-          </a>
-        </Link>
+        <Actions />
       </div>
     </>
   );
