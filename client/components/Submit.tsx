7d503eacc86488d10e5d68968b83e6ecf37fb23a
Fábio Matias <fabio_mcm2@hotmail.com>
2022-02-07 00:35:53 +0000

    refactor(ui): extract button styles

--- client/components/Submit.tsx
+++ client/components/Submit.tsx
@@ -1,8 +1,12 @@
 import formStyles from "../styles/Form.module.css";
+import buttonStyles from "../styles/Button.module.css";
 
 export default function Submit() {
   return (
-    <button className={formStyles.submit} type="submit">
+    <button
+      className={[formStyles.submit, buttonStyles.button].join(" ")}
+      type="submit"
+    >
       &#9658;
     </button>
   );
