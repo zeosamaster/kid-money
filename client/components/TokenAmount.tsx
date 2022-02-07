acfa84a0d1529f138184f3fe5b47b673c2845a89
Fábio Matias <fabio_mcm2@hotmail.com>
2022-02-07 01:34:59 +0000

    feat(ui): display token balance

--- client/components/TokenAmount.tsx
+++ client/components/TokenAmount.tsx
@@ -4,9 +4,33 @@ interface Props {
   value: ethers.BigNumber;
 }
 
-export default function TokenAmount({ value }: Props) {
-  const numericValue = value.toNumber();
-  const formattedValue = numericValue.toFixed(2);
+const suffixes = ["", "k", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc"];
+export function abbreviateCommifiedAnswer(commifiedAnswer: string): string {
+  // split answer's orders of magnitude
+  const splitAnswer = commifiedAnswer.split(",");
 
-  return <span>$KM {formattedValue}</span>;
+  // answers lower than a billion don't need abbreviating
+  if (splitAnswer.length < 2) {
+    return (Math.floor(Number(commifiedAnswer) * 100) / 100).toFixed(2);
+  }
+
+  // get the correct order of magnitude suffix
+  const [shortPart, decimals] = splitAnswer;
+  const suffix = suffixes[splitAnswer.length - 1];
+
+  // abbreviate with order of magnitude suffix
+  const abbreviatedAnswer = `${shortPart}.${decimals.slice(0, 2)}${suffix}`;
+
+  return abbreviatedAnswer;
+}
+
+export default function TokenAmount({ value }: Props) {
+  const withoutDecimalPrecision = ethers.utils.formatUnits(value, 18);
+  const commifiedValue = withoutDecimalPrecision.replace(
+    /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
+    ","
+  );
+  const abbreviatedValue = abbreviateCommifiedAnswer(commifiedValue);
+
+  return <>$KM {abbreviatedValue}</>;
 }
--- client/components/Wallet.tsx
+++ client/components/Wallet.tsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { TokenContext } from "../context/TokenContext";
 import { WalletContext } from "../context/WalletContext";
 import walletStyles from "../styles/Wallet.module.css";
 import { ConnectWallet } from "./ConnectWallet";
+import TokenAmount from "./TokenAmount";
 import WalletAddress from "./WalletAddress";
 
 export default function Wallet() {
   const { account } = React.useContext(WalletContext);
+  const { balance } = React.useContext(TokenContext);
 
   return (
     <div className={walletStyles.container}>
       {account === null && <ConnectWallet />}
       {account !== null && <WalletAddress address={account} />}
+
+      {balance !== null && (
+        <p className={walletStyles.tokenAmount}>
+          <TokenAmount value={balance} />
+        </p>
+      )}
     </div>
   );
 }
