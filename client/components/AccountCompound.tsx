ab2216cadd3694d5f44e1f5071c04579c6d71ebc
Fábio Matias <fabio_mcm2@hotmail.com>
2022-02-10 11:50:46 +0000

    feat(ui): display compound data

--- client/components/AccountCompound.tsx
+++ client/components/AccountCompound.tsx
@@ -3,13 +3,43 @@ import { BankContext } from "../context/BankContext";
 import styles from "../styles/Home.module.css";
 import { withForm } from "../utils/form";
 import Form from "./Form";
+import Loading from "./Loading";
+import TokenAmount from "./TokenAmount";
 
 export default function AccountCompound() {
-  const { compound } = React.useContext(BankContext);
+  const { accountInfo, bankConfigs, compound } = React.useContext(BankContext);
+
+  const compoundPercentage = bankConfigs?.compoundPercentage;
+  const maxCompoundReturn = bankConfigs?.maxCompoundReturn;
+  const savings = accountInfo?.savings;
+  const compoundReturn =
+    savings && compoundPercentage
+      ? savings.mul(compoundPercentage).div(100)
+      : null;
 
   return (
     <div className={styles.accountSection}>
       <h2>Compounding</h2>
+
+      <p className={styles.compoundPercentage}>
+        {!compoundPercentage && <Loading />}
+        {compoundPercentage && (
+          <>
+            Compound percentage: {compoundPercentage}% (
+            {compoundReturn && <TokenAmount value={compoundReturn} />})
+          </>
+        )}
+      </p>
+
+      <p className={styles.maxCompoundReturn}>
+        {!maxCompoundReturn && <Loading />}
+        {maxCompoundReturn && (
+          <>
+            Max compound return: <TokenAmount value={maxCompoundReturn} />
+          </>
+        )}
+      </p>
+
       <div className={styles.grid}>
         <div className={styles.card} style={{ gridColumn: "1 / span 2" }}>
           <h3>💰 Compound</h3>
